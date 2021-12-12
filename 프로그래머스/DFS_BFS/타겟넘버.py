3ce3b932638573ce7d105d009a82bae6328ce5a0
1000yj <37067204+cheonyeji@users.noreply.github.com>
2021-12-13 00:09:17 +0900

    dfs로 해결

---
+++ 프로그래머스/DFS_BFS/타겟넘버.py
@@ -0,0 +1,19 @@
+answer = 0
+
+
+def dfs(numbers, target, sum, depth):
+    global answer
+    if depth == len(numbers):
+        if sum == target:
+            answer += 1
+        return
+    dfs(numbers, target, sum + numbers[depth], depth + 1)
+    dfs(numbers, target, sum - numbers[depth], depth + 1)
+
+
+def solution(numbers, target):
+    dfs(numbers, target, 0, 0)
+    return answer
+
+
+print(solution([1, 1, 1, 1, 1], 3))
