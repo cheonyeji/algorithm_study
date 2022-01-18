ccef276436017a8e5f3ea54eb930a5924e3e6c34
1000yj <37067204+cheonyeji@users.noreply.github.com>
2022-01-18 22:08:30 +0900

    Create 실전5_효율적인화폐구성.py
    - 풀이를 참고
    - 점화식을 세워야 문제를 풀 수 있다고 생각하고 계속 경우의 수를 봤는데 잘못된 접근이었다.
    - 화폐 단위를 훑으면서, 해당 화폐단위로 구성되는 경우 min연산을 통해 갱신하면 되는 문제였다

---
+++ 이코테/DP/실전5_효율적인화폐구성.py
@@ -0,0 +1,22 @@
+# 2022-01-18
+# 이코테 ch8 다이나믹 프로그래밍 실전 문제 5 효율적인 화폐 구성
+
+# 아예 점화식 자체를 접근을 못했고, 불필요하게 수들을 더하고 뺐다
+# 앞에서부터 차근차근 봤어야 했는데 또 차근차근을 못했다.... 끙
+
+n, m = map(int, input().split())
+money = [int(input()) for _ in range(n)]
+
+d = [10001] * (m + 1)
+
+d[0] = 0
+
+for i in range(n):
+    for j in range(money[i], m + 1):
+        if d[j - money[i]] != 10001:
+            d[j] = min(d[j], d[j - money[i]] + 1)
+
+if d[m] == 10001:
+    print(-1)
+else:
+    print(d[m])
