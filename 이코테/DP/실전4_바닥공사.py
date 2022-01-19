7c4931ce0f8e92b946644eb925b5c315e77105cc
1000yj <37067204+cheonyeji@users.noreply.github.com>
2022-01-19 14:21:43 +0900

    백준 제출해보고 오류 잡은 코드

--- 이코테/DP/실전4_바닥공사.py
+++ 이코테/DP/실전4_바닥공사.py
@@ -1,15 +1,16 @@
 # 2022-01-18
 # 이코테 ch8 다이나믹 프로그래밍 실전 문제 4. 바닥 공사
+# https://www.acmicpc.net/problem/11727
 
 # 작은 것부터 차근차근!!!
 
 n = int(input())
-d = [0] * 1000
+d = [0] * 1001
 
 d[1] = 1
 d[2] = 3
 
 for i in range(3, n + 1):
-    d[n] = d[n - 1] + d[n - 2] * 2
+    d[i] = d[i - 1] + d[i - 2] * 2
 
 print(d[n] % 796796)
