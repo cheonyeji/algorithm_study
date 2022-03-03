62595fa13948f5246a242d25fd940ed6d218b9e3
1000yj <37067204+cheonyeji@users.noreply.github.com>
2022-03-03 19:42:47 +0900

    Create 13458.py
    - 삼성 기출
    - 테스트 케이스들을 먼저 보고 내가 이해한 것이 맞는지 먼저 체크해보고 코드를 작성하는 습관을 들이자

---
+++ 백준/13458.py
@@ -0,0 +1,36 @@
+# 2022-03-03
+# 삼성 기출 문제 - 백준 13458 시험 감독
+# https://www.acmicpc.net/problem/13458
+
+import math
+
+n = int(input())
+students = list(map(int, input().split()))
+b, c= map(int, input().split())
+
+total = 0
+
+# if b<=c:
+#     for i in students:
+#         if i > c:
+#             total += (i // c + math.ceil(i%c))
+#         # 부감독관 한명으로 충분
+#         else:
+#             total += 1
+# else:
+#     for i in students:
+#         if i > b:
+#             total += (1 + (i-b)//c + math.ceil((i-b)%c))
+
+#         # 총감독관 한명으로 충분
+#         else:
+#             total += 1
+
+# 무조건 총감독관은 한명 박아둬야함
+for i in students:
+    if i>b:
+        total += (1 + (i-b)//c + (1 if (i-b)%c != 0 else 0))
+    else:
+        total += 1
+
+print(total)
