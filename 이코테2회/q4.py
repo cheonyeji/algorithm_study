a50f9dd7acf68a47dbbaf94133e25e7cc35ebdc3
1000yj <37067204+cheonyeji@users.noreply.github.com>
2022-04-17 18:21:31 +0900

    Create q4.py
    - 2시간 고민해보고 해설 참고
    - 가장 작은 양수 1부터 만들 수 있나 체크하는데, 그 뒤에 다음 숫자를 만들 수 있는 것을 단순히 앞에서부터 더해가면서 체크하면 된다는 것을 캐치하지 못했음
    - 적어놓은 풀이법을 참고하기

---
+++ 이코테2회/q4.py
@@ -0,0 +1,35 @@
+# 2022-04-17
+# 이코테 2회차 그리디 Q4
+
+"""
+현재 상태에서 매번 좋은 것만 선택하기
+현재 상태 : 1부터 target-1까지의 모든 금액을 만들 수 있는 상태
+-> 매번 target 금액을 만들 수 있는지 체크 (=현재 확인하는 동전의 단위가 target 이하인지)
+"""
+
+
+n = int(input())
+coins = list(map(int, input().split()))
+
+coins.sort()
+
+target = 1
+
+for i in coins:
+    if i <= target:
+        target += i
+    else:
+        break
+
+print(target)
+
+
+"""
+TC 1 -> 8
+5
+3 2 1 1 9
+
+TC 2 -> 7
+4
+1 2 3 8 
+"""
