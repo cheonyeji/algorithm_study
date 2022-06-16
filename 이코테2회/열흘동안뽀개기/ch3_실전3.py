0d79e5cd5d6c3347be8e69a798c2ab92ab87b76e
1000yj <37067204+cheonyeji@users.noreply.github.com>
2022-06-16 15:06:37 +0900

    Create ch3_실전3.py
    각 행 입력받을 때마다 min값 저장해두고 그 min값 중 max 값 출력하면 ok

---
+++ 이코테2회/열흘동안뽀개기/ch3_실전3.py
@@ -0,0 +1,13 @@
+# 2022-06-16
+# 이코테 열흘동안 뽀개기 프로젝트 1일차
+# 그리디 실전 문제 3 숫자 카드 게임
+# 소요 시간 :
+
+n, m = map(int, input().split())
+
+min_val = -1
+for i in range(n):
+    data = list(map(int, input().split()))
+    min_val = max(min_val, min(data))
+
+print(min_val)
