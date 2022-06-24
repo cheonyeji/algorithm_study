918e8c893bcd354de0742b7b4f72b6b5f2c84b99
1000yj <37067204+cheonyeji@users.noreply.github.com>
2022-06-24 16:04:50 +0900

    소요시간 업데이트

--- 이코테2회/열흘동안뽀개기/ch4_예제1.py
+++ 이코테2회/열흘동안뽀개기/ch4_예제1.py
@@ -1,20 +1,22 @@
 # 2022-06-24
 # 이코테 열흘동안 뽀개기 2일차
 # 구현 예제 1 상하좌우
-# 소요 시간 :
+# 소요 시간 : 20분
+
 
 def move(r, c, case, N):
-    if case == 'L' and c > 2:
-        return (r, c-1)
-    elif case == 'R' and c < N:
-       return (r, c+1)
-    elif case == 'U' and r > 2:
-        return (r-1, c)
-    elif case == 'D' and r < N:
-        return (r+1, c)
-    
+    if case == "L" and c > 2:
+        return (r, c - 1)
+    elif case == "R" and c < N:
+        return (r, c + 1)
+    elif case == "U" and r > 2:
+        return (r - 1, c)
+    elif case == "D" and r < N:
+        return (r + 1, c)
+
     # 어떤 경우에도 안 걸리는 경우
-    return (r, c) 
+    return (r, c)
+
 
 N = int(input())
 move_data = list(input().split())
@@ -27,4 +29,3 @@ for m in move_data:
 
 
 print(r, c)
-
