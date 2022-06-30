befab9f503528eaa9bb640400df4f60a2874c78c
1000yj <37067204+cheonyeji@users.noreply.github.com>
2022-06-30 18:39:10 +0900

    Update 3진법뒤집기.py
    참고하면 좋을 코드 추가
    int('숫자로이루어진문자열', 진법) -> 10진법으로 변환

--- 프로그래머스/level1/3진법뒤집기.py
+++ 프로그래머스/level1/3진법뒤집기.py
@@ -3,6 +3,19 @@
 # https://programmers.co.kr/learn/courses/30/lessons/68935
 # 소요 시간 : 18:24 ~ 18:35 (10m)
 
+# 참고하면 좋을 다른 사람 코드
+"""
+def solution(n):
+    tmp = ''
+    while n:
+        tmp += str(n % 3)
+        n = n // 3
+
+    # int('숫자(혹은 16진법의 문자와 같은 문자)로 이루어진 문자열',해당 진법) -> 10진법의 수로 변환
+    answer = int(tmp, 3) 
+    return answer
+"""
+
 # 10진법 -> 3진법인데 거꾸로 들어감
 def convert10to3(n):
     result = ""
