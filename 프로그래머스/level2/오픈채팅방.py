5640540db2786520ba32662fb75da82077e02d5e
cheonyeji <ssaltuck@gmail.com>
2022-07-17 19:04:21 +0900

    2019 카카오 기출 - 나가고 새로운 닉네임으로 입장한 경우 먼저 닉네임 변경처리를 해주고 입장처리를 해야 문제가 없음. (변경된 닉네임으로 먼저 입장했다고 answer에 추가하고 change해주게 되면 변경된 닉네임과 예전 닉네임 사이 겹치는 문자열이 존재했을때 에러가 발생)

--- 프로그래머스/level2/오픈채팅방.py
+++ 프로그래머스/level2/오픈채팅방.py
@@ -1,4 +1,53 @@
 # 2022-07-17
 # 프로그래머스 Lv2 - 오픈채팅방
 # https://school.programmers.co.kr/learn/courses/30/lessons/42888
-#
+# 소요시간 : 18:04 ~ 19:01 (57m)
+
+# key : uid
+# value : [ 현재닉네임, [answer몇번째에 있는지 인덱스값들] ]
+user_list = {}
+
+
+def Change(uid, name, answer):
+    # 이미 등장한 적이 있는 유저가 닉네임을 바꿀 것
+    for idx in user_list[uid][1]:
+        answer[idx] = answer[idx].replace(user_list[uid][0], name)
+
+    user_list[uid][0] = name
+
+
+def Enter(uid, name, answer):
+    # 이미 입장했던 기록이 있는 유저라면
+    if uid in user_list:
+        # 나갔다가 닉 바꿔서 들어온거면 예전에 있던거 싹 다 닉네임 바꿔주기
+        if user_list[uid][0] != name:
+            Change(uid, name, answer)
+        # 그러고 나서 입장처리
+        user_list[uid][1].append(len(answer))
+
+    # 첫번째 입장인 경우
+    else:
+        user_list[uid] = [name, [len(answer)]]
+
+    answer.append(name + "님이 들어왔습니다.")
+
+
+def Leave(uid, answer):
+    # 무조건 등장한 적이 있는 유저가 떠날 것
+    user_list[uid][1].append(len(answer))
+    answer.append(user_list[uid][0] + "님이 나갔습니다.")
+
+
+def solution(record):
+    answer = []
+
+    for r in record:
+        data = r.split(" ")
+        if data[0] == "Enter":
+            Enter(data[1], data[2], answer)
+        elif data[0] == "Leave":
+            Leave(data[1], answer)
+        else:
+            Change(data[1], data[2], answer)
+
+    return answer
