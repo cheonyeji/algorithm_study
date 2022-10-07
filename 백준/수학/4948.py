01b480a12f6c22bddaa1b00796235cd7efc80a13
cheonyeji <ssaltuck@gmail.com>
2022-10-07 19:00:59 +0900

    에라토스테네스의 체 자연수 n개 미만의 소수 개수를 구하는 알고리즘

---
+++ 백준/수학/4948.py
@@ -0,0 +1,32 @@
+# 2022-10-07
+# week1. 수학
+# https://www.acmicpc.net/problem/4948
+# 소요시간 : 18:34 ~
+
+import math
+
+# 에라토스테네스의 체 방식 사용
+def cntPrimeNums(start, end):
+    # 1~end까지
+    erato = [True] * end
+
+    # 1은 소수가 아니므로 체크
+    erato[0] = False
+
+    for i in range(2, int(math.sqrt(end)) + 1):
+        if erato[i] == True:
+            for i in range(i + i, end, i):
+                erato[i] = False
+    cnt = 0
+    for i in range(start, end):
+        if erato[i] == True:
+            cnt += 1
+    return cnt
+
+
+n = -1
+while True:
+    n = int(input())
+    if n == 0:
+        break
+    print(cntPrimeNums(n + 1, 2 * n + 1))
