fa722e8aedf82c9f9bb56ea6b2f36dfbe48d2b6e
cheonyeji <ssaltuck@gmail.com>
2022-10-10 14:53:57 +0900

    math.gcd() 함수를 사용하여 최대공약수 구하기. 파이썬 애스터리스트는 컨테이너 데이터 unpacking을 지원한다!

---
+++ 백준/week1수학/17087.py
@@ -0,0 +1,19 @@
+# 2022-10-10
+# week1 - 수학. 숨바꼭질 6
+# https://www.acmicpc.net/problem/17087
+# 소요시간 : 14:25~ 14:47 (m)
+
+import math
+
+
+n, s = map(int, input().split())
+
+data = list(map(int, input().split()))
+
+gaps = []
+
+for v in data:
+    gaps.append(abs(s - v))
+
+
+print(math.gcd(*gaps))
