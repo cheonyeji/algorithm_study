ae98172de8182f3f6e445f5aa4402d7dbc97b6b1
cheonyeji <ssaltuck@gmail.com>
2022-10-13 14:06:06 +0900

    너무 어렵게 생각했다. 지금까지 몇개의 수가 들어갔는지만 체크해주면 로직이 매우 간단해지는 문제였다. 단순 구현 문제 맞다..>!

--- 백준/week2자료구조/1874.py
+++ 백준/week2자료구조/1874.py
@@ -1,7 +1,7 @@
-# 2022-10-12
+# 2022-10-12, 2022-10-13
 # week2 - 자료구조. 스택 수열
 # https://www.acmicpc.net/problem/1874
-# 소요시간 : 16:25 ~ 17:35 (70m, 시간초과), 집가서 추가로 더 보기 (m)
+# 소요시간 : 16:25 ~ 17:35 (70m, 시간초과), 12:47 ~ 14:00 (70m, 해설 진짜 조금 참고)
 import sys
 
 input = sys.stdin.readline
@@ -9,11 +9,72 @@ input = sys.stdin.readline
 N = int(input())
 DATA = [int(input()) for _ in range(N)]
 
+count = 0
+answer = []
+nums = []
+for num in DATA:
+    while count < num:
+        answer.append("+")
+        count += 1
+        nums.append(count)
+    top = nums.pop()
+    if top == num:
+        answer.append("-")
+    else:
+        answer.clear()
+        break
+
+if len(answer) == 0:
+    print("NO")
+else:
+    for i in answer:
+        print(i)
+
+
+"""
+# 삽질 1. 왜 그리 꼬아 생각하는지..! 코드를 범용적으로 써야지
+max_top = 0
+answer = []
+nums = []
+for num in DATA:
+    if len(nums) == 0:
+        for i in range(max_top, num + 1):
+            if i == 0:
+                continue
+            nums.append(i)
+            answer.append("+")
+        nums.pop()
+        answer.append("-")
+        max_top = num
+    else:
+        top = nums.pop()
+        if top == num:
+            answer.append("-")
+        elif top < num:
+            nums.append(top)
+            for i in range(max_top + 1, num + 1):
+                nums.append(i)
+                answer.append("+")
+            nums.pop()
+            answer.append("-")
+            max_top = num
+        else:
+            answer = []
+            break
+
+if len(answer) == 0:
+    print("NO")
+else:
+    for i in answer:
+        print(i)
+"""
+
+"""
+# 삽질 2. 경우의 수를 나눌 필요가 없었다. count라는 변수를 선언하여 해결.
 nums = []
 answer = []
 
 isPopped = [False] * (N)
-
 
 def calcul():
     for num in DATA:
@@ -54,3 +115,4 @@ if len(result) == 0:
 else:
     for i in result:
         print(i)
+"""
