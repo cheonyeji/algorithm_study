df03033391f9ffac2f77ad85dc4b192a066cb244
cheonyeji <ssaltuck@gmail.com>
2023-04-17 16:39:54 +0900

    collections의 defaultdict()를 사용하면 보다 간단하게  풀 수 있었다

--- 프로그래머스/고득점kit/dfs_bfs/여행경로.py
+++ 프로그래머스/고득점kit/dfs_bfs/여행경로.py
@@ -3,8 +3,42 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/43164
 # 소요 시간 : 14:46 ~ 16:09 (80m) -> 테케 통과 후 50% 통과해서 해설 참고
 
-# 첫번째 시도한 코드 (절반만 정답인 코드...)
+from collections import defaultdict
+
+# 해설 참고 코드 (많이 접근했는데 너무 꼬아서 생각해서 예외에 걸린듯!)
 def solution(tickets):
+    path = []
+
+    # key - value 형태인데 시작점 : [도착점1, 도착점2] 형태로 그래프 생성
+    graph = defaultdict(list)  # 리스트 형태로 딕셔너리 초기화
+    for (start, end) in tickets:
+        graph[start].append(end)
+
+    # 도착점 리스트 역순 정렬 (알파벳 순으로 먼저 가기 위해)
+    for v in graph.keys():
+        graph[v].sort(reverse=True)
+
+    # "ICN"부터 시작
+    stack = ["ICN"]
+
+    # dfs로 모든 노드 순회하기
+    while stack:
+        top = stack.pop()
+
+        # top요소가 그래프에 없거나, top 요소를 시작점으로 하는 티켓이 없는 경우 path에 저장
+        if top not in graph or not graph[top]:
+            path.append(top)
+        # top을 다시 스택에 넣고 top의 도착점 중 가장 마지막 지점을 꺼내와 스택에 저장(티켓 사용)
+        else:
+            stack.append(top)
+            stack.append(graph[top].pop())
+
+    # 방문한 순서 역순으로 반환
+    return path[::-1]
+
+
+# 첫번째 시도한 코드 (절반만 정답인 코드...)
+def solution2(tickets):
     answer = []
     airports = []
 
